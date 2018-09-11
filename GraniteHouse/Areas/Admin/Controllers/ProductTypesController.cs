0cf6f9df8a30ccfd5d783cbe86d1998d38478966
Shazad Ishaque <shazad_ishaque@yahoo.co.uk>
2018-09-11 14:58:09 +0100

    Added ProductType.Edit view's get and post methods

--- GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -34,9 +34,47 @@ namespace GraniteHouse.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(ProductTypes productTypes)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _db.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productTypes);
+        }
+
+        //GET Edit Action Method
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+
+            if(productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        //POST Edit Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ProductTypes productTypes)
+        {
+            if(id != productTypes.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Update(productTypes);
                 await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
