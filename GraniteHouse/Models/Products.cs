c9b845752755872c31f06a87a1fda07bd39e94f8
Shazad Ishaque <shazad_ishaque@yahoo.co.uk>
2018-09-11 16:47:42 +0100

    Added Products Model

---
+++ GraniteHouse/Models/Products.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public bool Available { get; set; }
+
+        public string Image { get; set; }
+
+        public string ShadeColour { get; set; }
+
+        [Display(Name="Product Type")]
+        public int ProductTypeId { get; set; }
+
+        [ForeignKey("ProductTypeId")]
+        public virtual ProductTypes ProductTypes { get; set; }
+
+        [Display(Name="Special Tag")]
+        public int SpecialTagId { get; set; }
+
+        [ForeignKey("SpecialTagId")]
+        public virtual SpecialTags SpecialTags { get; set; }
+    }
+}
