a61d789b56267204769e7052ae7f32c315b82d9c
Shazad Ishaque <shazad_ishaque@yahoo.co.uk>
2018-09-12 14:00:40 +0100

    Added Appointments and ProductSelectedForAppointsments Models and View Model to combine them. Added migration to create models in the database Added Shopping Cart Controller with Index action method

---
+++ GraniteHouse/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Extensions;
+using GraniteHouse.Models;
+using GraniteHouse.Models.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraniteHouse.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ShoppingCartController : Controller
+    {
+        protected readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ShoppingCartViewModel ShoppingCartVM { get; set; }
+        public object Product { get; private set; }
+
+        public ShoppingCartController(ApplicationDbContext db)
+        {
+            _db = db;
+            ShoppingCartVM = new ShoppingCartViewModel()
+            {
+                Products = new List<Models.Products>()
+            };
+        }
+
+        //Get: Index Shopping Cart
+        public async Task<IActionResult> Index()
+        {
+            List<int> listShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+
+            if(listShoppingCart.Count > 0)
+            {
+                foreach(int cartItem in listShoppingCart)
+                {
+                    Products product = _db.Products.Include(p => p.SpecialTags).Include(p => p.ProductTypes).Where(p => p.Id == cartItem).FirstOrDefault();
+                    ShoppingCartVM.Products.Add(product);
+                }
+            }
+
+            return View(ShoppingCartVM);
+        }
+    }
+}
---
+++ GraniteHouse/Models/ViewModel/ShoppingCartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models.ViewModel
+{
+    public class ShoppingCartViewModel
+    {
+        public Appointments Appointments { get; set; }
+        public List<Products> Products { get; set; }
+    }
+}
