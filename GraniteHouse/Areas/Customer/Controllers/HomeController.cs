f73b4345d45210c32a212eadcd9734e89e5842d0
Shazad Ishaque <shazad_ishaque@yahoo.co.uk>
2018-09-12 11:43:40 +0100

    Updated Home.Index view

--- GraniteHouse/Areas/Customer/Controllers/HomeController.cs
+++ GraniteHouse/Areas/Customer/Controllers/HomeController.cs
@@ -1,19 +1,30 @@
-﻿using System;
+﻿using GraniteHouse.Data;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GraniteHouse.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraniteHouse.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var productList = await _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags).ToListAsync();
+
+            return View(productList);
         }
 
         public IActionResult About()
